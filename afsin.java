2fceaaf4e2172b470c759f7cc1abe32332b5474f
Taslima Ahmed <taslimascience@gmail.com>
2018-03-31 22:45:57 +0600

    Add files via upload

---
+++ afsin.java
@@ -0,0 +1,16 @@
+public class afsin{
+	 public static void main(String[]args){
+		int sum=0;
+		 for(int i=0; i<args; i++){
+			 sum+=Integer.parseInt(args[i]);
+		 }
+		    System.out.println(+sum);
+	 
+	 
+	 
+	 
+	 }
+	
+	
+	
+}
